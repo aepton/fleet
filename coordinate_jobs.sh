e6d134966c55f82f565c441e9833d06ca12206c7
Abraham Epton <abraham.epton@socrata.com>
2017-05-15 15:20:02 -0700

    Updating coordinate-job script with basic implementation

---
+++ coordinate_jobs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+/home/ubuntu/code/fleet/fetch_sqs.sh
+
+job=$(</etc/current_job)
+instance=$(ec2metadata --instance-id | cut -d " " -f 2)
+
+if [ "$job" == "degree days" ]; then
+  /home/ubuntu/code/degree_days/runner.sh
+elif [ "$job" == "WA ETL" ]; then
+  /home/ubuntu/code/campfin/scripts/rebuild_prod.sh
+elif [ "$job" == "WA dedupe" ]; then
+  /home/ubuntu/code/campfin/scripts/run_dedupe.sh
+elif [ "$job" == "" ]; then
+  aws ec2 stop-instances --instance-ids=$instance
+fi
+
