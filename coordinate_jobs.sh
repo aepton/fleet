d85c7d7bbcbb0e7585cb83b87e3001c8e2fb6fc7
Abraham Epton <abraham.epton@socrata.com>
2017-05-28 14:06:41 -0700

    Making startup script update repos

--- coordinate_jobs.sh
+++ coordinate_jobs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+get_current_instance() {
+  instance=$(ec2metadata --instance-id | cut -d " " -f 2)
+}
+
 get_current_job() {
   /home/ubuntu/code/fleet/fetch_job.sh
   job=$(</etc/current_job)
-}
-
-get_current_instance() {
-  instance=$(ec2metadata --instance-id | cut -d " " -f 2)
 }
 
 shutdown_instance() {
@@ -15,6 +15,17 @@ shutdown_instance() {
   sleep 2m
   # aws ec2 stop-instances --instance-ids=$instance
 }
+
+update_repos() {
+  repos=( "/home/ubuntu/code/campfin" "/home/ubuntu/code/campfin_admin" "/home/ubuntu/code/fleet" )
+  for i in "${repos[@]}"
+  do
+    cd $i
+    git pull origin master
+  done
+}
+
+update_repos
 
 get_current_job
 get_current_instance
